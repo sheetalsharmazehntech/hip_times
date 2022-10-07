0be8d19dbb52c84e0abb3782a7538cd465462c7a
sheetalsharmazehntech <sheetal.sharma@zehntech.com>
2022-10-07 17:58:20 +0530

    add some application code

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,34 @@
+class ItemsController < ApplicationController
+
+def index
+    @item = Item.all.order("created_at DESC")
+end
+
+def new
+@item = Item.new
+end
+
+def show
+    @item = Item.find(params[:id])
+end
+
+def create
+    @item = Item.new(item_params)
+    if @item.save
+        redirect_to root_path
+    else
+        render'new'
+    end
+end
+
+def destroy
+end
+
+private
+
+def item_params
+ params.require(:item).permit(:title, :description)
+end
+
+
+end
